a9636dd7f8e077d5a174efd9f6db1ee56a50507d
Luca Holzer <luca.holzer@knapp.com>
2023-03-30 15:59:16 +0200

    Deleted files which are now encapsulated into "ExcelLikeDataGrid", added utility for connection string

--- WpfApplication/Utilities/Util.cs
+++ WpfApplication/Utilities/Util.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,37 +10,43 @@ using System.Windows.Media;
 
 namespace WpfApplication.Utilities
 {
-    internal static class Util
+    public static class Util
     {
-        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
-        {
-            if (depObj != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-                {
-                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
 
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
-                }
+        /// <summary>
+        /// Builds the partial connection string to a full one holding username and password in runtime and returns it.
+        /// </summary>
+        /// <param name="connStringKey"></param>
+        /// <param name="userName"></param>
+        /// <param name="userPassword"></param>
+        /// <returns>The full connectionString</returns>
+        /// <exception cref="KeyNotFoundException"></exception>
+        static string BuildConnectionString(string connStringKey, string userName, string userPassword)
+        {
+            // Retrieve the partial connection string named databaseConnection
+            // from the application's app.config or web.config file.
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[connStringKey];
+
+            if (null != settings)
+            {
+                // Retrieve the partial connection string.
+                string connectString = settings.ConnectionString;
+                Console.WriteLine("Original: {0}", connectString);
+
+                // Create a new SqlConnectionStringBuilder based on the
+                // partial connection string retrieved from the config file.
+                SqlConnectionStringBuilder builder =
+                    new SqlConnectionStringBuilder(connectString);
+
+                // Supply the additional values.
+                builder.UserID = userName;
+                builder.Password = userPassword;
+                Console.WriteLine("Modified: {0}", builder.ConnectionString);
+                return builder.ConnectionString;
             }
+            throw new KeyNotFoundException("Key \"" + connStringKey + "\" not found.");
         }
 
-        public static childItem FindVisualChild<childItem>(DependencyObject obj)
-            where childItem : DependencyObject
-        {
-            foreach (childItem child in FindVisualChildren<childItem>(obj))
-            {
-                return child;
-            }
-
-            return null;
-        }
     }
 }
